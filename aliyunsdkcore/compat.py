eca706d60992a2ee3545b820ac3d92f3301af00e
Luo Chunhui <wo@luochunhui.com>
2017-06-18 19:30:45 +0800

    pydev warning

--- aliyunsdkcore/compat.py
+++ aliyunsdkcore/compat.py
@@ -1,7 +1,7 @@
 try:
-    from urllib.request import pathname2url
-    from urllib.parse import urlencode
-    import http.client as httplib
+    from urllib.request import pathname2url  # @UnusedImport
+    from urllib.parse import urlencode  # @UnusedImport
+    import http.client as httplib  # @UnusedImport
 except:
     from urllib import pathname2url, urlencode
     import httplib
